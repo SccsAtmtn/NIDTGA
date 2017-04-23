a192c9ecac815b2e97ea3e0b2a7c220d7d3751ae
Zhitao Li <lizt1994@163.com>
2017-04-23 18:39:26 +0800

    add configuration file of database

--- src/nid_management.c
+++ src/nid_management.c
@@ -9,6 +9,17 @@ int main() {
     struct sockaddr_in6 clientAddress;
     struct NIDRequest request;
     struct NIDResponse response;
+    char dbUser[21], dbUserPasswd[21], dbName[21], dbTableName[21];
+
+    FILE *fin = fopen("/home/sccsatmtn/database/NIDdb.conf", "r");
+    fgets(dbUser, 20, fin);
+    fgets(dbUserPasswd, 20, fin);
+    fgets(dbName, 20, fin);
+    fgets(dbTableName, 20, fin);
+    fclose(fin);
+
+    dbUser[strlen(dbUser)-1] = dbUserPasswd[strlen(dbUserPasswd)-1] = '\0';
+    dbName[strlen(dbName)-1] = dbTableName[strlen(dbTableName)-1] = '\0';
 
     serverSockfd = socket(AF_INET6, SOCK_STREAM, 0);
     serverAddress.sin6_family = AF_INET6;
@@ -29,11 +40,11 @@ int main() {
         
         MYSQL myConnection;
         mysql_init(&myConnection);
-        while (!mysql_real_connect(&myConnection, "localhost", "sccsatmtn", "secret", "NIDManagement", 0, NULL, 0)) {
+        while (!mysql_real_connect(&myConnection, "localhost", dbUser, dbUserPasswd, dbName, 0, NULL, 0)) {
             printf("Connection failed\n");
         }
         char sql[250];
-        sprintf(sql, "SELECT nid FROM NIDUserInfo WHERE nid=\'%s\' and passwd=\'%s\'", request.nid, request.passwd);
+        sprintf(sql, "SELECT nid FROM %s WHERE nid=\'%s\' and passwd=\'%s\'", dbTableName, request.nid, request.passwd);
         int res = mysql_query(&myConnection, sql); 
         if (res) {
             printf("SELECT error %d: %s\n", mysql_errno(&myConnection), mysql_error(&myConnection));
